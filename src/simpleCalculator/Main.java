693f688c34ac631ea1aee2ea0ba5a3ae9214ec56
JustGinger888 <ethanmoffatt888@icloud.com>
2020-11-14 22:46:19 +0000

    Calc

---
+++ src/simpleCalculator/Main.java
@@ -0,0 +1,19 @@
+package simpleCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter A Numeric Vale: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter A Numeric Vale: ");
+        double num2 = scanner.nextDouble();
+
+        double answer = num1/num2;
+        System.out.println(answer);
+    }
+
+}
